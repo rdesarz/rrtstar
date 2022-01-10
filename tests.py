22bbebc6db84f88aabd09c3820553015dba7ddf7
Romain Desarzens <hib.pub@protonmail.com>
2022-01-10 22:22:19 +0000

    Update unit tests

--- tests.py
+++ tests.py
@@ -1,22 +1,21 @@
 import unittest
 
-import main
+from main import Point2d, Vertex, constant_speed_line_steering_policy
 
 
 class MyTestCase(unittest.TestCase):
-    def test_generate_new_sample(self):
-        point = main.generate_new_sample(main.Zone2d(x_min=-10.0, x_max=10.0, y_min=-10.0, y_max=10.0))
-        print(point)
+    def test_constant_line_steering_policy(self):
+        start = Vertex(position=Point2d(x=0.0, y=0.0), trajectory=[], parent = None, cost=1.0)
+        dest = Point2d(x=3.0, y=0.0)
+        time = 1.0
+        velocity = 0.1
 
-    def test_compute_nearest(self):
-        graph = main.Tree(vertices=[main.Vertex(position=main.Point2d(x=2.0, y=2.0), connected=[]),
-                                    main.Vertex(position=main.Point2d(x=3.0, y=3.0), connected=[])])
-        point = main.Point2d(x=3.0, y=3.0)
+        cost, traj = constant_speed_line_steering_policy(start, dest, time, velocity)
 
-        nearest = main.compute_nearest_euclidian_distance(point, graph)
-
-        self.assertEqual(nearest, graph.vertices[1].position)
+        self.assertEqual(cost, 1.1)
+        self.assertEqual(traj.path[0], start.position)
+        self.assertEqual(traj.path[-1], Point2d(x=0.1, y=0.0))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
