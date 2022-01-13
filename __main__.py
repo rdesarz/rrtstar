4b3cedc4c94f755a10cfb60e4027c4db06d05fd1
Romain Desarzens <hib.pub@protonmail.com>
2022-01-13 09:07:25 +0100

    Remove obstacles and use uniform sampling

--- __main__.py
+++ __main__.py
@@ -5,28 +5,25 @@ from nearest import compute_nearest_euclidian_distance
 from params import Parameters, Environment
 from plot import update_plot
 from rrt_star import update_tree, Vertex, Tree
-from sampling import generate_new_sample_biased_towards_goal
+from sampling import generate_new_sample_uniform
 from steering import constant_speed_line_steering_policy
 
 
 def main():
     # Initialize environment
     planification_zone = Zone2d(x_min=0.0, x_max=10.0, y_min=0.0, y_max=10.0)
-    obstacles = [
-        RectangleObstacle(pos=Point2d(x=5.0, y=4.0), width=1.0, height=1.0),
-        RectangleObstacle(pos=Point2d(x=6.5, y=5.5), width=1.0, height=1.0),
-    ]
+    obstacles = []
     environment = Environment(
         planification_zone=planification_zone, obstacles=obstacles
     )
 
     # Set start and goal position
-    start = Point2d(x=2.0, y=2.0)
+    start = Point2d(x=5.0, y=5.0)
     goal = Point2d(x=8.0, y=6.0)
 
     # Set parameters
     parameters = Parameters(
-        max_nb_iterations=5000,
+        max_nb_iterations=1000,
         expand_dist=0.2,
         goal_sample_rate=20,
         path_sampling_step=0.05,
@@ -49,8 +46,8 @@ def main():
         return compute_nearest_euclidian_distance(new, tree)
 
     def sample_generation_policy():
-        return generate_new_sample_biased_towards_goal(
-            environment.planification_zone, goal, parameters.goal_sample_rate
+        return generate_new_sample_uniform(
+            environment.planification_zone
         )
 
     for iteration in range(0, parameters.max_nb_iterations):
