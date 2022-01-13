730e8337d7248d7cdbedb172dfca15b342fea850
Romain Desarzens <hib.pub@protonmail.com>
2022-01-13 09:00:48 +0100

    Add unit tests

--- tests/test_rrt_star.py
+++ tests/test_rrt_star.py
@@ -7,7 +7,7 @@ from geometry import Point2d
 
 class MyTestCase(unittest.TestCase):
     def test_constant_line_steering_policy(self):
-        start = Vertex(position=Point2d(x=0.0, y=0.0), trajectory=[], parent = None, cost=1.0)
+        start = Vertex(position=Point2d(x=0.0, y=0.0), trajectory=[], parent=None, cost=1.0)
         dest = Point2d(x=3.0, y=0.0)
         time = 1.0
         velocity = 0.1
@@ -17,6 +17,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(cost, 1.1)
         self.assertEqual(traj.path[0], start.position)
         self.assertEqual(traj.path[-1], Point2d(x=0.1, y=0.0))
+
 
 
 if __name__ == "__main__":
